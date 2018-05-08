11dcab8d2f4860d315b58eb6cafcb2b8659f5695
under <under@DESKTOP-NP5LPM4>
2018-05-08 23:57:37 +0700

    Add project files.

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public  class UserDao
+    {
+        private OnlineShopDbContext db = null;
+
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public User GetbyId(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+        public int Login(string userName, string passWord)
+        {
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);//&& x.Password == passWord);
+            if (result ==null)
+            {
+                return 0;//"Tài khoản không tồn tại";
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;//"Tài khoản bị khóa";
+                }
+                else
+                {
+                    if (result.Password == passWord)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using Model.Dao;
+using OnlineShop.Areas.Admin.Models;
+using OnlineShop.Common;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.UserName, Encrytor.MD5Hash(model.PassWord));
+                if (result == 1)
+                {
+                    var user = dao.GetbyId(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserId = user.ID;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản đang bị khóa");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Mật khẩu không đúng");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập lỗi");
+                }
+            }
+            return View("Index");
+
+
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
